1c7cddb941cd7c75680b0d27b328495160f10b07
219124701 <ongeyane96@gmail.com>
2024-05-31 08:27:06 +0200

    update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-/*============meniIcon====*/
+
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Get references to the menu icon and the navigation menu
-  const menuIcon = document.getElementById('menuIcon');
-  const navbar = document.querySelector('.navbar');
+    // Get references to the menu icon and the navigation menu
+    const menuIcon = document.getElementById('menuIcon');
+    const navbar = document.querySelector('.navbar');
 
-  // Add click event listener to the menu icon
-  menuIcon.addEventListener('click', function() {
-      // Toggle the 'active' class on the navigation menu
-      navbar.classList.toggle('active');
-  });
+    // Add click event listener to the menu icon
+    menuIcon.addEventListener('click', function() {
+        // Toggle the 'active' class on the navigation menu
+        navbar.classList.toggle('active');
+    });
 });
 
 
