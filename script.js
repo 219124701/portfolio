8d25b6b3f20e2cb0b747fcde45609b9998371240
219124701 <ongeyane96@gmail.com>
2024-05-31 09:44:34 +0200

    update

--- script.js
+++ script.js
@@ -49,31 +49,33 @@ function toggleLightMode() {
 }
 
 
-function sendMessage() {
-    (function () {
-      emailjs.init("xSpbX9MCq3BRrB199"); // Account Public key
-    })();
-  
-    var params = {
-      FullName: document.querySelector("#FullName").value,
-      Email: document.querySelector("#Email").value,
-      Subject: document.querySelector("#Subject").value,
-      Mobile: document.querySelector("#Mobile").value,
-      Message: document.querySelector("#Message").value
-    };
-  
-    var serviceID = "service_sr97xy8"; // Email Service ID
-    var templateID = "template_7fi70m6"; // Email template ID
-  
-    emailjs.send(serviceID, templateID, params)
-      .then(res => {
-        alert("Email sent successfully");
-        // clear the form
-        document.getElementById('contact-form').reset();
-      })
-      .catch(err => {
-        alert("Failed to send email: " + JSON.stringify(err));
-      });
-  }
 
+function sendMessage() {
+  (function () {
+    emailjs.init("xSpbX9MCq3BRrB199"); // Account Public key
+  })();
+
+  var params = {
+    FullName: document.querySelector("#FullName").value,
+    Email: document.querySelector("#Email").value,
+    Subject: document.querySelector("#Subject").value,
+    Mobile: document.querySelector("#Mobile").value,
+    Message: document.querySelector("#Message").value
+  };
+
+  console.log(params); // Log the params to check if data is collected correctly
+
+  var serviceID = "service_sr97xy8"; // Email Service ID
+  var templateID = "template_7fi70m6"; // Email template ID
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      alert("Email sent successfully");
+      // clear the form
+      document.getElementById('contact-form').reset();
+    })
+    .catch(err => {
+      alert("Failed to send email: " + JSON.stringify(err));
+    });
+}
 
